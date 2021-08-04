f9f2d7a4b83ef344b32e7d1abcff1c1e2c9f30b5
mohamad <msiysinyuy@gmail.com>
2021-08-04 17:11:11 +0100

    the routing system done

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,7 +1,10 @@
 import {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 function App() {
   // woring on the add button to toggle when a task is to be added
   const [showAddTask,setShowAddTask]=useState(false)
@@ -72,13 +75,21 @@ const fetchTask = async (id) =>{
 
 
   return (
+    <Router>
     <div className="container">
       <Header  onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd = {addTask}/>}
-      {tasks.length>0? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>: 'No Task To Show'}
-
+        <Route path='/'exact render={(props)=>(
+        <>
+        {showAddTask && <AddTask onAdd = {addTask}/>}
+        {tasks.length>0? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>: 'No Task To Show'}
+      
+        </>
+      )}/>
+      <Route path='/about' component={About}/>
+      <Footer />
 
     </div>
+    </Router>
   );
 }
 
