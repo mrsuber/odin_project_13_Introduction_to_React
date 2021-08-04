e0e37fb3d69a27b8b73daf94da6848f8ffde6529
mohamad <msiysinyuy@gmail.com>
2021-08-04 12:29:08 +0100

    the react task app created and the button and header components made

---
+++ task-app/src/App.js
@@ -0,0 +1,22 @@
+
+import Header from './components/Header'
+function App() {
+  return (
+    <div className="container">
+      <Header  />
+
+    </div>
+  );
+}
+
+
+export default App;
+// for the class component, you need to import react
+// import React from 'react'
+
+// for the class based component
+// class App extends React.Component{
+//   render(){
+//     return<h1>hello form a class</h1>
+//   }
+// }
